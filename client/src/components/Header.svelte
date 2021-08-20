e64f8bd383b9267e93d0415d0b48dabe5d3c0511
Taisuke Fukuno <fukuno@jig.jp>
2021-08-21 08:49:33 +0900

    fix path

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -4,7 +4,7 @@
 
 <header class="flex justify-center items-center bg-gray-200 z-10">
   <div class="h-1/2 pr-5" on:click>
-    <Image src="/favicon.png" alt="icon" />
+    <Image src="./favicon.png" alt="icon" />
   </div>
   <h1
     class="font-mono text-4xl text-yellow-600 font-semibold italic tracking-widest"
